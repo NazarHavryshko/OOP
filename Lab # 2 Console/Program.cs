23a7a314b8cb90cf1a70332ddc1da23bd5b0c2df
unknown <nematod123@gmail.com>
2025-09-09 09:13:42 +0300

    Lab # 2: Add task # 3 console version

---
+++ Lab # 2 Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab___2_Console
+{
+    
+    internal class Program
+    {
+        static void input_int_number(ref int x)
+        {
+            string input_text;
+            input_text = Console.ReadLine();
+            bool status = true;
+
+
+
+            while (status)
+            {
+                if (int.TryParse(input_text, out x))
+                {
+                    status = false;
+                }
+                else
+                {
+                    Console.WriteLine("    Ви ввели некоректне значення спробуйте знову. ");
+                    input_text = Console.ReadLine();
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            bool status = true;
+            while (status) {
+                Console.WriteLine("Яке завдання ви хочете подивитися (3). Введіть 0 щоб завершити програму");
+                int task_number = -1;
+                input_int_number(ref task_number);
+
+                switch (task_number)
+                {
+                    case 3:
+                        task_3 task_3 = new task_3();
+                        task_3.start();
+                        break;
+                    case 0:
+                        status = false;
+                        break;
+                }
+            
+            }
+        }
+    }
+}
---
+++ Lab # 2 Console/task_3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab___2_Console
+{
+    internal class task_3
+    {
+        void input_number(ref double x)
+        {
+            string input_text;
+            input_text = Console.ReadLine();
+            bool status = true;
+
+
+
+            while (status)
+            {
+                if (double.TryParse(input_text, out x) && !input_text.Contains(","))
+                {
+                    status = false;
+                }
+                else
+                {
+                    Console.WriteLine("    Ви ввели некоректне значення спробуйте знову. ");
+                    input_text = Console.ReadLine();
+                }
+            }
+
+        }
+
+        public void start()
+        {
+            double x = 0, y = 0, z = 0;
+            Console.WriteLine("Введіть дробове значення x ");
+            input_number(ref x);
+            Console.WriteLine("Введіть дробове значення y ");
+            input_number(ref y);
+            Console.WriteLine("Введіть дробове значення z ");
+            input_number(ref z);
+
+            double s = Math.Round((Math.Pow(Math.Cos(z), 2) + (y / (Math.Pow(y, 2) - x))) / (Math.Pow((Math.Pow(x, 3) + Math.Log(x)), 0.25)), 4);
+          
+            Console.WriteLine($"S = {s}");
+
+        }
+
+    }
+}
